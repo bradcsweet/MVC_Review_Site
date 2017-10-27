4345a893914f8e4943e6c28e2604f4eb584df106
bradcsweet <bradcsweet@gmail.com>
2017-10-27 15:32:53 -0400

    populating properties

---
+++ MVC_Review_Site/Models/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Review_Site.Models
+{
+    public class Location
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "Body of Water")]
+        public string BodyOfWater { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+
+    }
+}
---
+++ MVC_Review_Site/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Review_Site.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Season { get; set; }
+
+
+        [ForeignKey("Location")]
+        public int LocationID { get; set; }
+        public virtual Location Location { get; set; }
+        
+    }
+}
