14dd032d643feccc1abe24321efe7a137f6d4941
bradcsweet <bradcsweet@gmail.com>
2017-10-28 14:58:45 -0400

    display names for review model

--- MVC_Review_Site/Models/Review.cs
+++ MVC_Review_Site/Models/Review.cs
@@ -12,12 +12,17 @@ namespace MVC_Review_Site.Models
         [Key]
         public int ID { get; set; }
         public DateTime Date { get; set; }
+        [Display(Name = "Condition of weather? ex. Sunny, Stormy, etc")]
         public string WeatherType { get; set; }
+        [Display(Name = "Condition of water? ex. Calm, Choppy, etc")]
         public string WaterCondition { get; set; }
+        [Display(Name = "Rate how easy to find eligible skipping stones. 1-10")]
         public int StoneAvailability { get; set; }
+        [Display(Name = "How many skips per throw did you average")]
         public int AverageSkip { get; set; }
-        [MaxLength(140)]
+        [MaxLength(140), Display(Name = "Any additional comments?")]
         public string Comment { get; set; }
+        [Display(Name = "Rate 1-10 of overall stone skipping experience")]
         public int OverallReview { get; set; }
 
 
